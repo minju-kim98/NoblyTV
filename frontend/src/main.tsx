0033ae3e7cfb0867cdf603194ad7b6bd6c8fd62c
minsangOh <flos9537@gmail.com>
2024-01-15 15:48:10 +0900

    🎨 : create and init folder

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2ed12a1457cd60b31c0f44f3ea59b8bbc2ddd4a49add3165228d093878fb7c24
-size 237
+oid sha256:0e56969468a501a0e7ffc1a0904c2edbdefc3682dee8d2e0fdc00a4a639610dc
+size 244
