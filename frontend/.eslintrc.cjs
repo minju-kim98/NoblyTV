e505d071c7a26a1f33c2a4d2ddf1901b3e6802d1
김민주 <ducco705@snu.ac.kr>
2024-01-17 15:18:12 +0900

    ♻ cleaned master branch
    Former-commit-id: 31f5234d320df252fceea59e8ecdb7b3313809e8

--- frontend/.eslintrc.cjs
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:85df2c311f47fb15db12c0af8a3ad7572aacce884b927b4ca90d8127408b1246
-size 867
--- frontend/src/pages/ContactUs/index.jsx
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:42f83a4a3952bc019c193c5d8dbdae6c47cf3b76d12b9725d82db4b38c0af29a
-size 1644
