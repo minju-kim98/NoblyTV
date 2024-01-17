ecdf9200fda5754456da64e8e5ef9dd61b53eff5
김민주 <ducco705@snu.ac.kr>
2024-01-17 15:33:36 +0900

    📝docs: Updated README.md
    Former-commit-id: 58457cf019e5c071057499d85c1031ba6ef6e7c0

--- TIL/minsangOh/TIL/240108/airbnb_style_guide_ESLint/vite.config.ts
+++ TIL/minsangOh/TIL/240108/airbnb_style_guide_ESLint/vite.config.ts
@@ -1,3 +1,7 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:3bba390990c78dbabc1a61123c18f30c76618666b4d9aa646bf03b96c2e52c53
-size 166
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+});
