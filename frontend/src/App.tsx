5f6c97f5d0fd1fea6c33076d92a751e765c26284
오민상 <flos9537@gmail.com>
2024-01-22 08:58:05 +0900

    💄style: add btn styles
    Former-commit-id: 10b6c23055d8a042b9dc524d7c565d6af2cbf625

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bb62284ee70b555c3e1f5a7bdd6f8c16f7d55a4dc41b7493c9b9a80b18e9f3c7
-size 136
+oid sha256:d65ab6ee44542ed4d985ff9b3a4c18121ea2bede6e6eaca693267451f20b9ad9
+size 142
