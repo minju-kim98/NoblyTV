e4afe058177911ae2f15c69b9277cc5f3240f1ad
이지호 <jeho1129@naver.com>
2024-01-22 17:57:00 +0900

    S10P12C103-262 docs:JOIN_OLD_MEMBER
    Former-commit-id: 1e44cf7e9fc503ab631611dada81948e90682ee4

---
+++ backend/project/src/main/java/BACKEND/project/domain/OldUserInfo.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ca2a6811272089fa518f7ddc3782f2dda161b2914dbfd1fece7c15492dd58d17
+size 1002
---
+++ backend/project/src/main/java/BACKEND/project/service/OldUserJoinService.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f774535e4810f831a75e6631c05b4f6a3e1752be3f627673a4d9916e445436c8
+size 952
