3fbc7ce0db34aa0b2e230d3a8bdd21ca9a2c0187
오민상 <flos9537@gmail.com>
2024-01-22 16:33:33 +0900

    ✨ feat: create login page
    Former-commit-id: b4d78ae49c2435dff842107163c7e20a9539a46c

--- frontend/src/pages/LogIn/SignUpPage.tsx
+++ frontend/src/pages/LogIn/SignUpPage.tsx
@@ -1,5 +1,3 @@
-function SignUpPage() {
-  return <div>SignUpPage</div>;
-}
-
-export default SignUpPage;
+version https://git-lfs.github.com/spec/v1
+oid sha256:767273375d0f5f70f86d095ff96cc157b44166d3a8d81e682c6602c02a8d7a56
+size 1426
