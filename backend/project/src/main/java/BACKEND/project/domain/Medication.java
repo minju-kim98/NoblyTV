88b0555ef19fb23657fe3aff7db4905ee7835f88
이지호 <jeho1129@naver.com>
2024-01-22 17:57:00 +0900

    S10P12C103-262 docs:JOIN_OLD_MEMBER
    Former-commit-id: 92ef8d0db494d88e30999e29096797734493ce41

---
+++ backend/project/src/main/java/BACKEND/project/domain/Medication.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:9672cccc2f3070c73a483b24e16de66d2372e3ef688dd081f7c5f2244fbc4d47
+size 472
---
+++ backend/project/src/test/java/BACKEND/project/repository/OldUserRepositoryTests.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d1d5c7d20eead48d008c3f01f47d84d59927175c7457ea8ac9ed01edefe25282
+size 1160
