756ecdcb74efe0e2b6a0960be73c755c4b31b077
이지호 <jeho1129@naver.com>
2024-01-23 16:34:31 +0900

    #S10P12C103-287 :sparkles:feat: View Diary
    Former-commit-id: ccb479a820fb5bc60fd05b963f43ed3e88190cbf

--- backend/project/src/main/java/BACKEND/project/repository/FamilyRelationRepository.java
+++ backend/project/src/main/java/BACKEND/project/repository/FamilyRelationRepository.java
@@ -1,9 +1,3 @@
-package BACKEND.project.repository;
-
-import BACKEND.project.domain.FamilyRelation;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FamilyRelationRepository extends JpaRepository<FamilyRelation, Long> {
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:515c0edb070bc2d481b449d972cecbba6453bed51d0c3ad9b3578a0c2b2bec71
+size 485
