f3dfcd69f9b467355d6ac907100b1314f73930df
오민상 <flos9537@gmail.com>
2024-01-23 13:49:49 +0900

    #S10P12C103-305 ♻️refactor: merge frontend src code and apply coding convention
    Former-commit-id: 499ec4edcb32fb4fb2e3959c85a9240d9bb4f460

--- frontend/src/pages/app/sign_up/name_id.tsx
+++ frontend/src/pages/app/sign_up/name_id.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:59f0493db808d6f7c19a93ef49fbfb682e9446e33219242c2ffa3caf95f95576
-size 1176
+oid sha256:2586e84a81ae980e04111b7ac53d68aa3f2b82ccfe61cd54b698338d7b3a28fc
+size 1218
