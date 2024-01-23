eafc3654fe8da2d347d9da71a4f6e533dbda12c2
오민상 <flos9537@gmail.com>
2024-01-23 11:54:55 +0900

    S10P12C103-362 🐛 fix: button, Modal type error fixed
    Former-commit-id: f01103b683ace46eb26ceb77bd373c1fa62d8273

--- frontend/src/components/modal/Modal.tsx
+++ frontend/src/components/modal/Modal.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2bc708305d99777d89b2b5770c35a8d3b1eed82f2765d6c2c706e9739d9e939e
-size 883
+oid sha256:26e598478954d112bc67a73d3b41fdc83878cc401676c473da937c591fd154b0
+size 926
