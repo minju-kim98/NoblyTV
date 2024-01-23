b3b4ed095523706ee1eb1acb0f02c9d0a2dbc904
이지호 <jeho1129@naver.com>
2024-01-23 14:45:08 +0900

    :sparkles: feat: Summaried Diary DB
    Former-commit-id: a181b828e0dc2a9fab420399afc98cdf6964fd3d

--- backend/project/src/main/java/BACKEND/project/domain/OldUserInfo.java
+++ backend/project/src/main/java/BACKEND/project/domain/OldUserInfo.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2fd83c6b8b1edb43ee66dcb11047257b5e702295c258a13538f8d91f41b80180
-size 1704
+oid sha256:0312dc74913b21fee6d25503ce00bdd58bffd2f67d592acddb468315251f14a5
+size 1856
--- backend/project/src/main/java/BACKEND/project/domain/OldUserRegistrationDto.java
+++ backend/project/src/main/java/BACKEND/project/domain/OldUserRegistrationDto.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:77b197a9bbaf8c68cd9131cddd89b31c2db0e55bf0891449bd67143c60cc1364
-size 888
+oid sha256:a0803379ff77227be4de30f641340221f2ee86f36f471a95a1679064c926f3a4
+size 764
