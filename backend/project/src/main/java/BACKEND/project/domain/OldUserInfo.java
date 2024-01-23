d85a5232917eec037bbc44f6ef9a0085b96548ea
이지호 <jeho1129@naver.com>
2024-01-23 15:10:31 +0900

    :bug: S10P12C103-286 fix: Summaried Diary Logic
    Former-commit-id: 89b9cc552d2cbad7bce8f8ee8b16430ad822819d

--- backend/project/src/main/java/BACKEND/project/domain/OldUserInfo.java
+++ backend/project/src/main/java/BACKEND/project/domain/OldUserInfo.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0312dc74913b21fee6d25503ce00bdd58bffd2f67d592acddb468315251f14a5
-size 1856
+oid sha256:a119bc31f60f7e65808a46de2f16890ec5ed3c7929731e64fc75977ae286582d
+size 1855
