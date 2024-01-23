80769ae4217a89d4d9e1705bccc28a929a93caeb
이지호 <jeho1129@naver.com>
2024-01-23 16:35:38 +0900

    #S10P12C103-287 :sparkles: feat: View Diary
    Former-commit-id: 9cd43dcc13c3d5b626038e9510d2d76c90af88ab

--- backend/project/src/main/java/BACKEND/project/domain/OldUserRegistrationDto.java
+++ backend/project/src/main/java/BACKEND/project/domain/OldUserRegistrationDto.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a0803379ff77227be4de30f641340221f2ee86f36f471a95a1679064c926f3a4
-size 764
+oid sha256:26d45561ec30d845307f4ba9fc644fcf782fb7021ce5f012b87b25737ba38d3b
+size 763
