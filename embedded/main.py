32abf224adc37652d97dc20b034673d4e78fb97e
송강산 <rkdtks0816@naver.com>
2024-01-23 08:49:51 +0000

    S10P12C103-376 ✨feat: In the process of developing the senior info page
    Former-commit-id: 4a62f203ddf61f8d0d1c8056cb4d9b707c48db85

--- embedded/main.py
+++ embedded/main.py
@@ -1,5 +1,3 @@
-def main():
-    print("Do something.")
-
-if __name__ == "__main__":
-    main()
+version https://git-lfs.github.com/spec/v1
+oid sha256:e453169efa8cfadd7dedafa57027f6e06ebe855351010e3d50b8b335409c224f
+size 78
---
+++ frontend/src/pages/app/sign_up/senior_conect.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:97f9585d051b346377b533395c8903f7bd9cea302d0ecbd867a0e9a3dcf6f6e3
+size 1593
