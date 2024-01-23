c58a2fadc2a8636c36d960eda3c3cc1881d38f3c
김민주 <ducco705@snu.ac.kr>
2024-01-23 13:25:37 +0900

    #S10P12C103-353 🐛 fix: fixed to run on Jetson nano
    Former-commit-id: 2d7cecfbea081dd327b402a481d305772b0d8a7e

--- embedded/video-summarization/main.py
+++ embedded/video-summarization/main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:df9ef386c15849c377693245cefa04c62db210bf03e22553b43b794e86d233d8
-size 2336
+oid sha256:72518f89de85a25cc1e9b3feca89efc1f2cc0c425b738f6ee3a737e9d531fad3
+size 2122
