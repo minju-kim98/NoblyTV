bff862ac1ef794301fde9d9017b9c99aa28033a2
현상균 <htts1049@gmail.com>
2024-01-24 18:00:04 +0900

    #S10P12C103-344 ✨feat: Send Prediction Data to Backend
    Former-commit-id: a18b391aa434e90870b16bacdd967c6aae7e1433

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,3 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './styles/index.css';
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+version https://git-lfs.github.com/spec/v1
+oid sha256:0e56969468a501a0e7ffc1a0904c2edbdefc3682dee8d2e0fdc00a4a639610dc
+size 244
