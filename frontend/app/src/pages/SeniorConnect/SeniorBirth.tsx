c861b495f99d9ee952afe290b1308203b055ecf4
오민상 <flos9537@gmail.com>
2024-01-25 15:39:07 +0900

    #S10P12C103-377✏️ comment: write coments
    Former-commit-id: f6690788bf79289ce5c0c688f71027c2d5ae3d8b

--- frontend/app/src/pages/SeniorConnect/SeniorBirth.tsx
+++ frontend/app/src/pages/SeniorConnect/SeniorBirth.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ce9144f0c3d723e9dc9c8bf6c5225c5d3782d536b4a441044a2e561fb9bec171
-size 3467
+oid sha256:7380d163a91b4d5d8efcb87f8585394a856e55543be9c527552f519c43e9f360
+size 3470
