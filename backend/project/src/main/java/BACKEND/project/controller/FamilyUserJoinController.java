6482d85d1ebc12b79ba3700db8ab66186974ba11
이지호 <jeho1129@naver.com>
2024-01-25 11:50:27 +0900

    #S10P12C103-273 :sparkles: feat: Modify an existing family member
    Former-commit-id: f84279a5a281338331e52c87b8d62e03bec1d4c9

--- backend/project/src/main/java/BACKEND/project/controller/FamilyUserJoinController.java
+++ backend/project/src/main/java/BACKEND/project/controller/FamilyUserJoinController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a6a6406ae85d0a78f7cdad156e4de5b78b244f3e43c8cc83e35e3336a8bf3ea7
-size 1661
+oid sha256:6c1d7e0bf3d2d5177f6fe54be3f2c476b851d7cdee91665fbffb0ba7619a63d5
+size 2112
