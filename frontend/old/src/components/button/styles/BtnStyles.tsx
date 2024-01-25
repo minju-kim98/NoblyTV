67d30ed8f49a881ce487e472fa62d1dfedeb2a21
오민상 <flos9537@gmail.com>
2024-01-25 17:31:19 +0900

    🚚 rename: finished TVA, APP pjt separation
    Former-commit-id: 07b1e5b88c7cbeca2c7ec5063952293f52a4305b

--- frontend/old/src/components/button/styles/BtnStyles.tsx
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:66c553caa9af01e89b40e1b694434f5935e33c617cc14a02929fcb8fd65694ab
-size 932
--- frontend/tva/src/components/app/false_message.tsx
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:589f6f7bd9a43617965ee1451ce93b3ef1edb67b03cb8a0d0f5db17b1a86f59b
-size 519
