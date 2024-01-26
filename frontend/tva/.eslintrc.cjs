63cde10a52de5fd107c31afd09119dabc88c08f3
오민상 <flos9537@gmail.com>
2024-01-26 16:10:28 +0900

    #S10P12C103-421✨feat: create init styles and components files
    Former-commit-id: 924b19c86a9c941532472fd388003189f557c2e7

--- frontend/tva/.eslintrc.cjs
+++ frontend/tva/.eslintrc.cjs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7b1deae28855aaceabb41415165301096bb89772e26cc56422f8a82697482e07
-size 919
+oid sha256:89cbd3a6ced7820f8f8e07cf9ea8c4595811e695e1ba3b84bd5cf191c823eff2
+size 923
