ec79a660234aab4a2b8959c2d2455350ea32463b
강대수 <sore503@naver.com>
2024-01-26 16:11:42 +0900

    :sparkles: S10P12C103-272 :sparkles: olduser update
    Former-commit-id: 99d0a1ccc8b8639cc0a28551fe38c219cf6e1ef4

--- backend/project/src/main/java/BACKEND/project/service/OldUserJoinService.java
+++ backend/project/src/main/java/BACKEND/project/service/OldUserJoinService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:19584196724cbcd890dbedc4eaab78e57710898a9104de5240568df77d98197f
-size 3259
+oid sha256:b842be7302184aad36d8726c758db31a4241d353babeb0cb449b582974b896d3
+size 2414
