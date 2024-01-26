a7480503cf1ad3d40081b902550fc153bb9daae8
강대수 <sore503@naver.com>
2024-01-26 12:08:24 +0900

    :sparkles: S10P12C103-460 :sparkles: user checking
    Former-commit-id: 22a6ee791dc447be7a1823eaca6ec0839a6528af

--- backend/project/src/main/java/BACKEND/project/controller/OldUserJoinController.java
+++ backend/project/src/main/java/BACKEND/project/controller/OldUserJoinController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2eaf6daff7648c9ee01fc0417b32138ce32fae5999f6074645643a7a851658e8
-size 1361
+oid sha256:a7beda94007378db86caab1a1cb60083b46525d60aebb127e656acb0c4ebc9f9
+size 1627
--- backend/project/src/main/java/BACKEND/project/service/OldUserJoinService.java
+++ backend/project/src/main/java/BACKEND/project/service/OldUserJoinService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:43767baa79310fad4b90f80125c756493b35736cb1bfbc1618fd28db3a061ef7
-size 2287
+oid sha256:e82308ff566ed4e88b2dded46049b962353ce555775eb294bac401316ac2e0ba
+size 2460
