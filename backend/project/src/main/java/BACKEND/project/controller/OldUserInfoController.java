74c8ffdf9421952933e16b3b33c6bd3a48bec9de
강대수 <sore503@naver.com>
2024-01-26 16:57:50 +0900

    :sparkles: S10P12C103-272 :sparkles: olduser update(controller 부분 수정)
    Former-commit-id: 12bfdbd6f87c5415154024a47fcf88e79a5bdf3f

--- backend/project/src/main/java/BACKEND/project/controller/OldUserInfoController.java
+++ backend/project/src/main/java/BACKEND/project/controller/OldUserInfoController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:44972a315d65076fcc30fc6e20927e049a4d0e8698e5bbae3733e5ac91e48702
-size 981
+oid sha256:a0fa578afddb88dcdf2bcea6f6a0e7abeb5e71df0109e77b0b11192ec09f5bc1
+size 886
