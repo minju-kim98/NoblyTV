03c4807d6490d2d588b1c4e76b7a7340208ad13c
이지호 <jeho1129@naver.com>
2024-01-28 00:32:29 +0900

    #S10P12C103-249 :sparkles: feat: Family Member Logout FINISHED
    Former-commit-id: 8b143eba0c31fe94427163ccca36f62a06a97628

--- backend/project/src/main/java/BACKEND/project/service/FamilyLoginService.java
+++ backend/project/src/main/java/BACKEND/project/service/FamilyLoginService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c64794e4f437481a16df8c83d32fd2b4d2f21bf852d000e0eaef626ed4123124
-size 1458
+oid sha256:113a0abab4e0f022cffa8f86029d2983d4f0ed43170fbbfa2742e9bf14c00c0d
+size 1927
