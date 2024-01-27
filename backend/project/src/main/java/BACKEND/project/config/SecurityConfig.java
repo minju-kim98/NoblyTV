4d528dddd3219ab0f6b5c050e39aa614664fc351
이지호 <jeho1129@naver.com>
2024-01-28 00:32:29 +0900

    #S10P12C103-249 :sparkles: feat: Family Member Logout FINISHED
    Former-commit-id: f1c54d0b5e1eb628f9e3cf96232a3124e730c25a

--- backend/project/src/main/java/BACKEND/project/config/SecurityConfig.java
+++ backend/project/src/main/java/BACKEND/project/config/SecurityConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8cc5967c5fd589dbcd0f666e0ad16e9f44f68d916e8e7829aa89eb6a9d1283c2
-size 2219
+oid sha256:725735a6ff1698e71d3721c45f806c33942fbb78176a16d4e39c89c896e6c638
+size 2113
