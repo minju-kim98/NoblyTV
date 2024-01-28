865e235d075b5d48ed8f4cff09e21ead999a38d7
오민상 <flos9537@gmail.com>
2024-01-28 17:12:27 +0900

    #S10P12C103-421✨feat: finished_InstallPage and add package(qrcode.react)
    Former-commit-id: a1098232d3a442059bbeadb5a86c914d161241b2

--- frontend/tva/src/components/Install/styles/RightBoxStyle.tsx
+++ frontend/tva/src/components/Install/styles/RightBoxStyle.tsx
@@ -1,7 +1,3 @@
-import styled from 'styled-components';
-
-const RightBoxStyle = styled.div`
-  color: #ffffff;
-`;
-
-export default RightBoxStyle;
+version https://git-lfs.github.com/spec/v1
+oid sha256:a6afa63736c41eaef459dd5084d087b6199ddcd55463c22062b43abe4734f140
+size 286
--- frontend/tva/src/components/Install/styles/SeperationLineStyle.tsx
+++ frontend/tva/src/components/Install/styles/SeperationLineStyle.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e354a04e8a9a9d5a1c7acc6782a32963b06f95b1aed701d3b30b49e42a8b5aef
-size 167
+oid sha256:38262d9772e171870ad1365e88c7d2348f4fac3474869d0de63cbeee30081834
+size 210
