c8562f6db866f059bed293e7a1333584d3957986
오민상 <flos9537@gmail.com>
2024-01-29 09:06:59 +0900

    #S10P12C103-421✨feat: chaged css
    Former-commit-id: 9eba1d987f5bbde286d76c200d54f382898b6404

--- frontend/tva/src/components/Install/styles/LeftBoxStyle.tsx
+++ frontend/tva/src/components/Install/styles/LeftBoxStyle.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2785ecf15dd07a8a21e8416db5efeb85edf0b8a971acecc40337b7e5249d57b9
-size 416
+oid sha256:64361ec64b04ec92b76bfdd5229b2e2aeb710c69b0aa31637e6f82b4e52f9026
+size 478
