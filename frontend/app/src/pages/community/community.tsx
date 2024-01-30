8cc5466d4231453efde4fe36b48a613def8ebded
송강산 <rkdtks0816@naver.com>
2024-01-30 15:59:29 +0900

    #S10P12C103-368 ✨feet : create datetime
    Former-commit-id: f1ec490c41af1a7a46103a692bfa7471082e00a4

--- frontend/app/src/pages/community/community.tsx
+++ frontend/app/src/pages/community/community.tsx
@@ -1,13 +1,3 @@
-import Footer from '../../components/Footer/Footer';
-import Header from '../../components/Header/Header';
-
-function Community() {
-  return (
-    <div>
-      <Header />
-      <Footer />
-    </div>
-  );
-}
-
-export default Community;
+version https://git-lfs.github.com/spec/v1
+oid sha256:2e4f663863f9b16ab5084d0cbdfb4e0cb55a56c7be8b088932864c4095ed56cf
+size 540
---
+++ frontend/app/src/utils/manageAuthToken.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:663bb6493740071b9e7565f5b1276db20bea2175ef747240c796d99fdb9fb838
+size 653
