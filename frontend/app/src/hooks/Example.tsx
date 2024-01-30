cbc7ef4209f517cc097a192074800ac88b2ce90d
현상균 <htts1049@gmail.com>
2024-01-30 18:03:42 +0900

    #S10P12C103-478✨ feat: Started to Communicate with Frontend
    Former-commit-id: eff6dbd97ee14161e60fe17e6c205e366cad05af

--- frontend/app/src/hooks/Example.tsx
+++ frontend/app/src/hooks/Example.tsx
@@ -1,5 +1,3 @@
-function Example() {
-  return <div>Example</div>;
-}
-
-export default Example;
+version https://git-lfs.github.com/spec/v1
+oid sha256:41dd213a80542b3ba600f2a085e7bd9c57dfcd55d77adbe6fb443e49642958a9
+size 82
