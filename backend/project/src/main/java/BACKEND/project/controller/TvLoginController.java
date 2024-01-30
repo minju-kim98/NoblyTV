0ba7810db8aa6f865719903dd480a3b310db2147
이지호 <jeho1129@naver.com>
2024-01-30 22:03:42 +0900

    #S10P12C103-536 :recycle: refactoring: QR Code Login Logic
    Former-commit-id: 2bf7fb7817ee319d258f2461e89834940acb7070

--- backend/project/src/main/java/BACKEND/project/controller/TvLoginController.java
+++ backend/project/src/main/java/BACKEND/project/controller/TvLoginController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:92d5e16e8b3e6afb0afccf3267b6c90afb71c9cc549c4941986a7a27aed5f5a4
-size 1326
+oid sha256:6a646a25a4618ecbf0bb94717b101bfe854477063a6a2dc1a40f3eabfc477f3e
+size 979
