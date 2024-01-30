8e342d53948e9c2e00c3b77c7540e0c23a88c7f2
오민상 <flos9537@gmail.com>
2024-01-31 08:25:08 +0900

    #S10P12C103-373 ✏️ comment : modified url address
    Former-commit-id: b16b01c7f1cd0d7bb5d96c5555c0bfcd837452ae

--- backend/project/src/main/java/BACKEND/project/exception/InvalidCredentialsException.java
+++ backend/project/src/main/java/BACKEND/project/exception/InvalidCredentialsException.java
@@ -1,3 +1,7 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:7f366773d7c95e0c6fbeea21c47bf4114256283c4e4ee5e56aebbdb4e91b4922
-size 193
+package BACKEND.project.exception;
+
+public class InvalidCredentialsException extends RuntimeException {
+    public InvalidCredentialsException(String message) {
+        super(message);
+    }
+}
--- frontend/app/src/components/Qrcode/Qrcode.tsx
+++ frontend/app/src/components/Qrcode/Qrcode.tsx
@@ -1,3 +1,16 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:1ca06b50817699aa52bde1a8b382e5173446c25d1a177dd14d24db628e5e69ef
-size 393
+import { QrcodeContainerS, QrcodeTextS } from './QrcodeStyle';
+import QRCode from 'qrcode.react';
+
+function QrCode() {
+  const url = 'http://192.168.100.72:5173/senior-connect';
+  return (
+    <div>
+      <QrcodeContainerS>
+        <QRCode value={url} size={200} />
+      </QrcodeContainerS>
+      <QrcodeTextS>A9E9-L32B</QrcodeTextS>
+    </div>
+  );
+}
+
+export default QrCode;
