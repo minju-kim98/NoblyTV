ff5dba67aec2a02498dbdb66c3960d04737439db
송강산 <rkdtks0816@naver.com>
2024-01-30 10:26:23 +0900

    #S10P12C103-311 🐛fix : Merge blocked
    Former-commit-id: b8c30d9abb15267abf3b3372d431906b6345705f

--- frontend/app/src/components/Header/HeaderStyle.tsx
+++ frontend/app/src/components/Header/HeaderStyle.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2ca8706e2c5991f9bfab92bdf5881477e677f6939faf479a98403d4089b450a4
+oid sha256:69442662f277aade5b413bbde5153d59a17f8f47a4f81e1c270a2e537bbf85f2
 size 1307
