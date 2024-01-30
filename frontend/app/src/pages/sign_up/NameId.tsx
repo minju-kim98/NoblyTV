00a5756e63cce9bdd93007fb046091d941bb0ef4
오민상 <flos9537@gmail.com>
2024-01-31 08:25:08 +0900

    #S10P12C103-373 ✏️ comment : modified url address
    Former-commit-id: cce67f9c21b1480837a64cd30f6cd9c91f787e90

--- frontend/app/src/pages/sign_up/NameId.tsx
+++ frontend/app/src/pages/sign_up/NameId.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7f4f3753d3329fd782a0826b57ca406b80846bb89eded953743e44e99b6ccab3
-size 4132
+oid sha256:6a645b67e7cb45966b92b12859b72b4f96dec89f7fe7743d4ab851d025a58cd5
+size 4145
