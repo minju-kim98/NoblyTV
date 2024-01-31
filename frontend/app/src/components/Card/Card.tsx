6b672964db224093c73cc42b3cc22a1d8ee6ba64
오민상 <flos9537@gmail.com>
2024-01-31 14:51:56 +0900

    #S10P12C103-393✨ feat: finished community Card layout
    영상 커뮤니티의 카드(각각의 게시글)에 대한 레이아웃 작성 완료
    영상을 띄우는 로직 포함 X
    
    
    Former-commit-id: 80c12f1d36692128027e085d9d6834fcfec215f4

--- frontend/app/src/components/Card/Card.tsx
+++ frontend/app/src/components/Card/Card.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:58174daeda6a743b8dd3985e1dcb346111402f08bf86dcd5e1a434cdb0858fdc
-size 303
+oid sha256:c99030c0d4785958972e89ffe6e6f60f348026dd3291a6643ffc9591a5ab2b29
+size 314
