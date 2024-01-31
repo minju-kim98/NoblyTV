319f85c6ccc78856de47490cd2d3075282796f07
오민상 <flos9537@gmail.com>
2024-01-31 17:06:25 +0900

    #S10P12C103-631 🐛 fix:  code fix
    Former-commit-id: 8b4452b5be3e54e5c86e19daaace65044b2f331f

--- frontend/tva/src/App.tsx
+++ frontend/tva/src/App.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e71f959ef1c21b4c04a3d8c7ceaa0006a9f55a1ceda819c78280921ec3cd607e
-size 1042
+oid sha256:a82c7b11cd04de6ca61f9a27efb456ffe55ee55c9f0d8057dfff05ab6e4050a5
+size 723
