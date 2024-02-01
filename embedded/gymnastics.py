4c79b0728548e88447ad0e9ea78b8613192567ad
minju-kim98C:/Python312/python.exe <ducco705@snu.ac.kr>
2024-02-01 17:55:47 +0900

    🚚 move: changed folder name
    Former-commit-id: 803c1ccb1c0b9fc45b2fa7b7c09ccf44fd364dd2

---
+++ embedded/gymnastics.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:01dee53eaa68f89fad7138c37889985a6cb8ebda41185ac1d565a60ababa8250
+size 2238
---
+++ embedded/videoSummarization/v2021/summarizer/__init__.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:116ce4509c2a6e7917ae2e29368d06bcae4ed0ac94d68198826eff4c9b59be84
+size 64
