99e2d24201c063185761c45e61c8e97d47deeee9
minju-kim98 <ducco705@snu.ac.kr>
2024-02-01 22:38:02 +0900

    #S10P12C103-481 ✨ feat: gynmastic automation
    Former-commit-id: 17c52b3bd93468b0b478d14390667add32ed1a79

--- embedded/videoSummarization/main.py
+++ embedded/videoSummarization/main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e95eaecc2327f3b3c5c91cf2aaffafd157689d0ab85f512efed76b310cf393ee
-size 2219
+oid sha256:79a3b2fabcc375d2d48218a6bcec44b24ecc12369eee6611a1cbe8d3948072f7
+size 2482
