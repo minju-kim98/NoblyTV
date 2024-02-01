4538176bbb8674e6c975f6bfa4f5b2281b2e8630
minju-kim98 <ducco705@snu.ac.kr>
2024-02-02 08:30:58 +0900

    #S10P12C103-481 🚧 progress: working in recording
    Former-commit-id: e428c227de1f2dd9c389cd124964a678c12dc8dd

--- embedded/gymnastics.py
+++ embedded/gymnastics.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c6a4cb76f553e742354ea4022696bd18ee4b539a985455ccf9db12ec98e3fe98
-size 1424
+oid sha256:5e9fb9b5fcda0bafedafae47e3d5484f893dc5e1585fb0a774339f7475c043e6
+size 3049
