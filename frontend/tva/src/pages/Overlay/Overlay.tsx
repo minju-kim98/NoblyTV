c5818047a1fe1ec82e1d82057aefe17939e5aef3
오민상 <flos9537@gmail.com>
2024-02-01 11:15:42 +0900

    #S10P12C103-318 ✨ feat: add static files & add modal motions
    배경 영상 추가, 모달 움직임 변경(화면에는 하나의 모달창만 나오게 변경)
    
    
    Former-commit-id: c051008c9628d5c509424b9295801245ba0b0f10

--- frontend/tva/src/pages/Overlay/Overlay.tsx
+++ frontend/tva/src/pages/Overlay/Overlay.tsx
@@ -1,13 +1,3 @@
-import BgVideo from '../../components/BgVideo/BgVideo';
-import ChildModal from '../../components/ChildModal/ChildModal';
-
-function Overlay() {
-  return (
-    <div>
-      <BgVideo />
-      <ChildModal />
-    </div>
-  );
-}
-
-export default Overlay;
+version https://git-lfs.github.com/spec/v1
+oid sha256:b506d4d1694bb96a9acdc2c6a498373cd52e922766c389c6c476dad8a5202c7d
+size 857
