870220ff89d3a55029eefd9104772347394228c9
minju-kim98 <ducco705@snu.ac.kr>
2024-02-01 22:38:02 +0900

    #S10P12C103-481 ✨ feat: gynmastic automation
    Former-commit-id: 3b76dc9e5602fc4a438805025798f47e22020188

--- embedded/gymnastics.py
+++ embedded/gymnastics.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:01dee53eaa68f89fad7138c37889985a6cb8ebda41185ac1d565a60ababa8250
-size 2238
+oid sha256:c6a4cb76f553e742354ea4022696bd18ee4b539a985455ccf9db12ec98e3fe98
+size 1424
