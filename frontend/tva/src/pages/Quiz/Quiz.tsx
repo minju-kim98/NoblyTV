69c9e44653d34b7d1777567b998061ccadf8aba1
오민상 <flos9537@gmail.com>
2024-02-01 17:36:50 +0900

    #S10P12C103-635 ✨ feat: moving url
    퀴즈 끝나면 동영상나오는 url로 이동
    
    
    Former-commit-id: 7c104e8ed141be656618229a6e3d7793ec2fb7d4

--- frontend/tva/src/pages/Quiz/Quiz.tsx
+++ frontend/tva/src/pages/Quiz/Quiz.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e82cb0513cade7fb28569c4d995a23e3d18d7e5ba15b575a80c94814d13401cd
-size 874
+oid sha256:80970fa5a38d65cfdbb3976601e14da7adf4e404d25539b361e690f21ed18e68
+size 1105
