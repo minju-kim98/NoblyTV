6124274d222c5f87d0fe70a893101a72e40e6912
송강산 <rkdtks0816@naver.com>
2024-02-01 17:38:56 +0900

    #S10P12C103-408 ✨ feat : diary complete
    Former-commit-id: bfcd2b675fe0051573903dc8d9ff78e2aebe19a6

--- frontend/app/src/components/Modal/ModalType.tsx
+++ frontend/app/src/components/Modal/ModalType.tsx
@@ -1,3 +1,6 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:050a08fe8238a78f836be73d38362c9f35220d27fac7d8d602d149157178284b
-size 139
+interface ModalProps {
+  modalContents: React.ReactNode;
+  onClickBtn: (arg: React.ReactNode) => void;
+}
+
+export default ModalProps;
