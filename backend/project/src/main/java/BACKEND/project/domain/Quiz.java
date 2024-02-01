3d39b1faf77f9239ffff65f81530571e410546fd
이지호 <jeho1129@naver.com>
2024-02-01 09:59:40 +0900

    #S10P12C103-538 :sparkles: feat: Quiz DB
    Former-commit-id: d7c12e83c41c84c8066316494722a5a59a0187cf

---
+++ backend/project/src/main/java/BACKEND/project/domain/Quiz.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:326681ba0d858c1e1bd90faf23ee3b851f646d0e30b1c8b815a21e8f79a78d27
+size 480
---
+++ backend/project/src/main/java/BACKEND/project/dto/QuizDto.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d8586d648c58887114d868237af3fa254a0748006167f58076c31c1b3e2664cc
+size 245
