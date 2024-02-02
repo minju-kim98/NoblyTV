4fcef5de39ae4a2ce7f3a41a3dd5e548c0a320cb
오민상 <flos9537@gmail.com>
2024-02-02 16:45:54 +0900

    #S10P12C103-312 ✨ feat: writing  videoModal component
    MVP 발표 때 쓰인 코드
    
    
    Former-commit-id: d76802939c1daec3781cadbd3db043d09fe9a25f

--- frontend/tva/src/App.tsx
+++ frontend/tva/src/App.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:500f72dddcabc98cea3091e8a4f9cbcebaf7201562fba197f96e1ba361a4851d
-size 1056
+oid sha256:50b9336a5906a8b5d9f0a537dbdd2fb4d49cffec7022996e37b626449be54f57
+size 1161
--- frontend/tva/src/pages/gymnastics/Gymnastics.tsx
+++ frontend/tva/src/pages/gymnastics/Gymnastics.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a61b93e7c9dae9122d4fadbacaf488fe1838bea8dfed2fd5ab2120de26db4253
-size 1626
+oid sha256:982e1e2f8255cb2b27018254d6ce4105b6481fe90910a80ecc90a15d28f65beb
+size 1296
