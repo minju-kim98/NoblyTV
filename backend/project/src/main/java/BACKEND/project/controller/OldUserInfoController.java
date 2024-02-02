a344f475586cee0be6f714bb597935d0ebbe06f0
이지호 <jeho1129@naver.com>
2024-02-02 13:30:07 +0900

    #S10P12C103-637 :recycle: refactoring: EDITED GETTING OLD_USER_INFO LOGIC(FINAL)
    Former-commit-id: 9d8b39581a2bdcd19a8238fb344fd4c1e8c1c869

--- backend/project/src/main/java/BACKEND/project/controller/OldUserInfoController.java
+++ backend/project/src/main/java/BACKEND/project/controller/OldUserInfoController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:73c1ba6bbe76d7041a468a2f9eed46a5401e1b60fcbb3b66b497c411f6666e98
-size 1315
+oid sha256:ba5e4254bf22643a9f0a929bb9e79957206f6fde5b206c5b2601565928a92ab6
+size 1241
--- backend/project/src/main/java/BACKEND/project/service/OldUserInfoService.java
+++ backend/project/src/main/java/BACKEND/project/service/OldUserInfoService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e21fd5bd6b61bf4d0282896719c0d1ee861205c77dc876f8646dd9ecb37a3647
-size 3736
+oid sha256:a1e50452a8916ae286a05bef5f73025d5f95390b6e061857d0400c8ccfd03c9c
+size 4091
