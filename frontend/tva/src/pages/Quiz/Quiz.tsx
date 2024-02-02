7e7e42ca256eb4fd8c7734f386865c0c0cda6019
오민상 <flos9537@gmail.com>
2024-02-02 10:52:55 +0900

    #S10P12C103-302 🐛 fix: Quiz export code fix
    Former-commit-id: 62da66c463b30747f1d49f13890ebae13b53ebbe

--- frontend/tva/src/pages/Quiz/Quiz.tsx
+++ frontend/tva/src/pages/Quiz/Quiz.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d950d25af76471726c9249fe45838b9e5d0ab6b699b14d87201b627bbcb3f64b
-size 1177
+oid sha256:3d06fab876cd52298863cc669e240c260e625aa5f867fc3d4e33f954e40b30ce
+size 1161
