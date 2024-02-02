6738c3f8bb258a348a874e6986f88518f44fb29c
이지호 <jeho1129@naver.com>
2024-02-02 13:30:07 +0900

    #S10P12C103-637 :recycle: refactoring: EDITED GETTING OLD_USER_INFO LOGIC(FINAL)
    Former-commit-id: c4bde14da738373b4ca1f52101bcea5a5574fa30

--- backend/project/src/main/java/BACKEND/project/service/OldUserJoinService.java
+++ backend/project/src/main/java/BACKEND/project/service/OldUserJoinService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:43767baa79310fad4b90f80125c756493b35736cb1bfbc1618fd28db3a061ef7
-size 2287
+oid sha256:aeb94dfd5ebd814201e4d4e96a30324cbf5b4d347f393affac2cd0df0263d2d6
+size 2342
