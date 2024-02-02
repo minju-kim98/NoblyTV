e15c9f7601f220c0d807268405c493c561cb6020
강대수 <sore503@naver.com>
2024-02-02 11:28:38 +0900

    S10P12C103-543  :sparkles: feat: Gymnastics develop
    Former-commit-id: ce00245d843de002c6ee0efed53922b3cbe7d2a0

--- backend/project/src/main/java/BACKEND/project/domain/FamilyUserInfo.java
+++ backend/project/src/main/java/BACKEND/project/domain/FamilyUserInfo.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a4ac10acaa75a2c1891bbe2fc5a4fb073899f8cf0e61c094debf57fd61395249
-size 1228
+oid sha256:f040b472823a5e1e2b11d194aded8321c3db01746201105e1fbb435a14d527d8
+size 1331
