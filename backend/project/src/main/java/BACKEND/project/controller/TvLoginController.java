966d6e498db63a11f6ca3fe404923b59fb7b0d5b
이지호 <jeho1129@naver.com>
2024-02-02 15:04:05 +0900

    #S10P12C103-645 :recycle: refactoring: EDITED GETTING FAMILY_USER_INFO FIELD
    Former-commit-id: 88cb0fe7cda013db8f8dd081d05426c285dc0c5c

--- backend/project/src/main/java/BACKEND/project/controller/TvLoginController.java
+++ backend/project/src/main/java/BACKEND/project/controller/TvLoginController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6a646a25a4618ecbf0bb94717b101bfe854477063a6a2dc1a40f3eabfc477f3e
-size 979
+oid sha256:279038023803e68ef602d27dae036bd0e98d350798a9605c6347931628b5b92c
+size 1245
--- backend/project/src/main/java/BACKEND/project/service/FamilyUserJoinService.java
+++ backend/project/src/main/java/BACKEND/project/service/FamilyUserJoinService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a1f9db2975276d902adb0d376ef7d4039e21fe897aa18efeb769aec2cbc9daa6
-size 7172
+oid sha256:2b4808d94397ba24b41dfa5f89ae7f33863369bd9c229f28ffa9bfb1dc6e7e0a
+size 7309
