49eabdcaeba3923459a7327dda96dd08b45e55ac
오민상 <flos9537@gmail.com>
2024-02-02 10:40:17 +0900

    #S10P12C103-302 ✨ feat: Diary and small talking  message view
    일기와 함께 간단한 대화를 위해 수신되는 데이터를 화면에 띄우는 기능 구현
    
    
    Former-commit-id: fc84db8e898fa579b3b6106b086f440faf9d8a14

--- frontend/tva/src/pages/Quiz/Quiz.tsx
+++ frontend/tva/src/pages/Quiz/Quiz.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:80970fa5a38d65cfdbb3976601e14da7adf4e404d25539b361e690f21ed18e68
-size 1105
+oid sha256:d950d25af76471726c9249fe45838b9e5d0ab6b699b14d87201b627bbcb3f64b
+size 1177
