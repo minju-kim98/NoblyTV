1dd4aee09957d6cad07e3d2547823648e97a22d2
minju-kim98C:/Python312/python.exe <ducco705@snu.ac.kr>
2024-02-02 14:38:55 +0900

    #S10P12C103-484 🐛 fix: fixed for MVP
    Former-commit-id: edcb31a444224f1ccd2533b27be2f03dde5b937a

--- embedded/videoSummarization/main.py
+++ embedded/videoSummarization/main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2ec1856c3cd1adad60010ef0dec899f36073bda83797eaa46928289520240a1c
-size 2488
+oid sha256:8434bb68b7d925f2eb21e80aab241a42bb77c08da474ed5a50d731443aa17cc4
+size 2547
