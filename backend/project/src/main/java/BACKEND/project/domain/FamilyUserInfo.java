64d52e2c05215ace0fffed7f23275ddde749b985
이지호 <jeho1129@naver.com>
2024-02-02 11:48:08 +0900

    #S10P12C103-637 :recycle: refactoring: EDITED GETTING OLD_USER_INFO LOGIC4
    Former-commit-id: 571575d945ee31e4cb29e8aedc795666ec7236a3

--- backend/project/src/main/java/BACKEND/project/domain/FamilyUserInfo.java
+++ backend/project/src/main/java/BACKEND/project/domain/FamilyUserInfo.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:46eecd93636abf757e666832c0ea94b854b6dadfeeca3b9a48e9102d73f4ad98
-size 1230
+oid sha256:f4aeeba60e5d68c6f7b574cad669c529d48f252496c081eff57626d28a0345f0
+size 1322
