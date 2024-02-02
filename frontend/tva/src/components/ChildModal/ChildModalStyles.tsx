3047045d32d17a52118606ed124e6f694c95b993
오민상 <flos9537@gmail.com>
2024-02-02 11:20:29 +0900

    #S10P12C103-312 ✨ feat: init videoModal component
    Former-commit-id: 44a8e0c4d54cfafbd785292d2625ac06c63bc0c3

--- frontend/tva/src/components/ChildModal/ChildModalStyles.tsx
+++ frontend/tva/src/components/ChildModal/ChildModalStyles.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c789a56fe55bcfdccc77dd78c12ff5ef78f88d2399a0528dfd8e0505d9f88afc
-size 1228
+oid sha256:eac8cfffa88e1fcb5348592e9fe5b66539f4db476535b7fe88e662da7b27df18
+size 1303
