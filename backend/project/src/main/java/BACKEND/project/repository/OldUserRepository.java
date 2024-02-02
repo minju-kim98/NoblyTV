21c68fd674cb9ff7c5cc387e8aa4b527f6d8120d
오민상 <flos9537@gmail.com>
2024-02-02 11:20:29 +0900

    #S10P12C103-312 ✨ feat: init videoModal component
    Former-commit-id: 77ea02f472e4145f18f17846e299ed4998cb3c0f

--- backend/project/src/main/java/BACKEND/project/repository/OldUserRepository.java
+++ backend/project/src/main/java/BACKEND/project/repository/OldUserRepository.java
@@ -1,16 +1,3 @@
-package BACKEND.project.repository;
-
-import BACKEND.project.domain.OldUserInfo;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import java.util.Optional;
-
-@Repository
-public interface OldUserRepository extends JpaRepository<OldUserInfo, Long> {
-    Optional<OldUserInfo> findByUserId(String userId);
-
-    OldUserInfo findByUsername(String username);
-}
-
-
+version https://git-lfs.github.com/spec/v1
+oid sha256:aa3515b071415129b5f37dfe618be9749bb342b1ab706728537dfe237c203eb7
+size 420
---
+++ frontend/tva/src/components/ChildModal/ViedoModal.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:1590ab7ad2aeacb094707628b5aee33374ab3f85f0420e4e37c9ad511a5cbc2d
+size 445
