2df567836fd6ba2a3c0e34ae78938271bbd3fe2c
오민상 <flos9537@gmail.com>
2024-02-02 10:59:44 +0900

    #S10P12C103-302 🐛 fix: modify soket disconnect code
    Former-commit-id: 895db4b847802dd25c9ef3f34addd825dbbd5e22

--- frontend/tva/src/pages/Quiz/Quiz.tsx
+++ frontend/tva/src/pages/Quiz/Quiz.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3d06fab876cd52298863cc669e240c260e625aa5f867fc3d4e33f954e40b30ce
-size 1161
+oid sha256:991f6d6561fd0bd2ba2f0d41272f110d19b2e90cdacf30a1d08168ced26d73b8
+size 1224
