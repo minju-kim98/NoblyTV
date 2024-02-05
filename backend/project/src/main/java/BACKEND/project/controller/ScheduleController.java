07364a6a3748c997de602ac7904090e9bf99836f
이지호 <jeho1129@naver.com>
2024-02-05 10:55:01 +0900

    #S10P12C103-544 :sparkles: feat: GET Schedule
    Former-commit-id: c87dea22de8957c4c404b84ca7579ab675ecd494

--- backend/project/src/main/java/BACKEND/project/controller/ScheduleController.java
+++ backend/project/src/main/java/BACKEND/project/controller/ScheduleController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5e0c3583e7afda9f1d9040ff95ac4a0930272d9fb078beac314412831280d820
-size 1011
+oid sha256:932e41ff00821bdeea1884a34a0021b3bc194ce67a95ffd0b65caf25b3c20205
+size 1339
