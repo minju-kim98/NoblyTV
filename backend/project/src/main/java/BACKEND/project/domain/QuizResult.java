a30b4e3767af4070a68c8d917ec21eb0027f8b2a
이지호 <jeho1129@naver.com>
2024-02-05 15:52:46 +0900

    #S10P12C103-595 :recycle: refactoring: GETTING OLD USER's INFORMATION
    Former-commit-id: a39084d700a4a46cab4a3749af203244dcc23e7a

--- backend/project/src/main/java/BACKEND/project/domain/QuizResult.java
+++ backend/project/src/main/java/BACKEND/project/domain/QuizResult.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:618adcdca7462e209fef9c104b7f43a7885467c770e64b942a1c836a8ffe19be
-size 558
+oid sha256:ea3010e051b93eba1ebed20074f0e50d0ae4266df829a99db5f66097e614e1a7
+size 663
