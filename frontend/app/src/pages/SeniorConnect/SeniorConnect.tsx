d9746a63519aad645195d86bcfad83a116b09679
송강산 <rkdtks0816@naver.com>
2024-02-05 17:51:53 +0900

    #S10P12C103-306 ✨ feat : login complete, gymnastics ~ing
    Former-commit-id: b76a03726c6cf276193ca888058f28c3c1b3cb48

--- frontend/app/src/pages/SeniorConnect/SeniorConnect.tsx
+++ frontend/app/src/pages/SeniorConnect/SeniorConnect.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:99e29f6a607e72eda63ae1170d68b85022c8146acd7ed121afb42a7bd10cd829
-size 2990
+oid sha256:17a3b7f39a4916d94101ae38f654939c49160af8f1d66251cea190358ed94204
+size 3855
