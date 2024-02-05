939133c9e7c57894f6173aabfcad1e47171c973a
이지호 <jeho1129@naver.com>
2024-02-05 16:13:27 +0900

    #S10P12C103-595 :recycle: refactoring: QUIZ UPDATE LOGIC
    Former-commit-id: e8c08ab742812d5904995bc415601c315932b1c1

--- backend/project/src/main/java/BACKEND/project/service/QuizService.java
+++ backend/project/src/main/java/BACKEND/project/service/QuizService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6bfb740f155de5141d8a1d324cc232de28ffc21545678049d0cb560ed6dca28a
-size 1754
+oid sha256:aac79c58ad8f258de24a0145327d2d47416d5306312bca2a4bc265b37c2c6f0d
+size 1869
