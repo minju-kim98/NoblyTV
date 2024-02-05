a75c1112270e33bae67ba242943c65a2c57ff305
김민주 <ducco705@snu.ac.kr>
2024-02-05 16:38:50 +0900

    #S10P12C103-653 🐛 fix: fixed CORS error for flask
    Former-commit-id: d4b97bbbbae07aab7af037d2e43ae80cde59d383

--- embedded/videoSummarization/videoCapture.py
+++ embedded/videoSummarization/videoCapture.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:eca52e612b0a9b08ad0d5880c3921a8c2630c90d976cdc8cc67b565d8d17160f
-size 1859
+oid sha256:c3aed74291d5497d5b1636417b96cd64064b2e086129ad4841c464018ca837cf
+size 1943
