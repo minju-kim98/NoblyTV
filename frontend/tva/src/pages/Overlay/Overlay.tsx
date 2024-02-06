350d181cdf6ccc3868435ba331ae700f069e4c3b
오민상 <flos9537@gmail.com>
2024-02-06 17:48:14 +0900

    #S10P12C103-664 ✨ feat: Gymnastic motion bug fix and ad news videos
    뒤도 돌려서 확인중
    
    
    Former-commit-id: 81628c455fba8cea9ea585b1fcfa51bf3965928d

--- frontend/tva/src/pages/Overlay/Overlay.tsx
+++ frontend/tva/src/pages/Overlay/Overlay.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:eb8ba54569105eaa6b6513504578e38b8f8d490c9848aa733d812a86df35e873
-size 3144
+oid sha256:5131cdbd010157b85d82d8f42600dc72edfca6d0c8b8a19ef45c5e2301ad026d
+size 1642
