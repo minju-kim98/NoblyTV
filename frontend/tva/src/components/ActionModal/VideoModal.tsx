08d8780096ba088d1c49afe5cc47380ba9909072
오민상 <flos9537@gmail.com>
2024-02-06 14:02:27 +0900

    #S10P12C103-442 ✨ feat: slide moving Videomodal
    1. 모달창 등장과 확대, 사라짐을 슬라이드로 움직이도록 구현
    2. app.tsx 정리 및 더미 파일 삭제
    3. 모달창 사라질 때 5초 지연 뒤에 사라지는 것으로 변경
    
    
    Former-commit-id: bd16a5325eef6238f213242d74275c3c714ba822

--- frontend/tva/src/components/ActionModal/VideoModal.tsx
+++ frontend/tva/src/components/ActionModal/VideoModal.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e3129510e0171e9eaa8e2f757cd7944c9c876694117f95a3f7c6dcebfe6b9894
-size 1340
+oid sha256:b0c7a7ced1c7247dcc7d5377c8150922d6169cdd360c7a9b1834c581ae082355
+size 1755
--- frontend/tva/src/pages/Overlay/Overlay_R2.tsx
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:727d83e6611f5795ffcc57b1011ad11ab12adab46e57e6fcd83c0a2df6e53432
-size 1503
