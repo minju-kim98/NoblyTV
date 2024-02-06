632a0910332f858b8acbb3d4a4c31cfdb0e9017b
오민상 <flos9537@gmail.com>
2024-02-06 15:51:10 +0900

    #S10P12C103-664 ✨ feat: Gymnastic motion bug fix and ad news videos
    Former-commit-id: f2e1e0e19f20e027f626a31960b9d2263ec90f68

--- frontend/tva/src/components/ActionModal/QuizModal.tsx
+++ frontend/tva/src/components/ActionModal/QuizModal.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:47c39578b4adaaef553c41a1e41ae053102d038ab88605f21a4bd39a146cd0b8
-size 1204
+oid sha256:7957022329a3bfa09adfc1180f44e41aacc8a1ac3b71d70db2354b8c818d0289
+size 1205
