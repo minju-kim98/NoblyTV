06a1e595d58a1b7c17d9b23635d7acc53297fd59
오민상 <flos9537@gmail.com>
2024-02-07 16:12:27 +0900

    #S10P12C103-671 ✨ feat: change from VideoModal to FamilyVideoModal
    모호한 이름 대신 명시적으로 컴포넌트 이름 변경 및 관련 로직 추가
    
    
    Former-commit-id: a4ffc028d3dfde2e688c578f2e31072113074132

--- frontend/tva/src/components/ActionModal/ModalSwitcher.tsx
+++ frontend/tva/src/components/ActionModal/ModalSwitcher.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5a06f59de6ac42e9131a2f195509ce43550a9837df6292b83deb4a16ce83131e
-size 627
+oid sha256:28f37964ca8cf99240259927f402df79603b7e528ef0eda3c1fe0de7a5f3aefb
+size 728
