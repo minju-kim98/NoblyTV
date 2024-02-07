82a6cfe12d079b746553bf45adf073ee779547d7
오민상 <flos9537@gmail.com>
2024-02-07 17:28:16 +0900

    #S10P12C103-314 ✨ feat: init. selected display (string or url)
    가족 영상 모달 컴포넌트에서 일반 문자열일 때 출력과 url일때 출력하는 방법을 다르게 가져가는 로직 추가
    
    
    Former-commit-id: 57cdda4915ca2e364c819607e44b6df52f60c8e3

--- frontend/tva/src/App.tsx
+++ frontend/tva/src/App.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6abe77cd1a72ae5ec1dfacd62d001200a32d503e646a5d74deeae34d8a34fcdb
-size 1221
+oid sha256:831a4f1f260ce61c2ee37ed1cce06f44e5ef0e2c5465767abe9d523076c523b6
+size 1216
