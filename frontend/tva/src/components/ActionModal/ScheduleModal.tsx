e485200be2e1710671ce4e4bd5cfc0d845ad20d0
오민상 <flos9537@gmail.com>
2024-02-07 16:21:25 +0900

    #S10P12C103-671 ✨ feat: confirm schedule test
    동작 테스트 이상 x
    
    
    Former-commit-id: fb7f47baa09bae2c08bb51aff8cf2f4186521d70

--- frontend/tva/src/components/ActionModal/ScheduleModal.tsx
+++ frontend/tva/src/components/ActionModal/ScheduleModal.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:84c5463ee8e96cb27b9f83c2ed1a8364f9a8da3013363ab0101ae3592472bec8
-size 1428
+oid sha256:f3c923612e5137c58460c7bbed305e02d87e6d75926604edf1b85e8801706c47
+size 1374
