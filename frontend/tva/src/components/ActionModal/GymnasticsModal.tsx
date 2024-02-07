70b198547611eab4ba28f5af1175b6fb567775e6
오민상 <flos9537@gmail.com>
2024-02-07 15:13:58 +0900

    #S10P12C103-656 ✨ feat: modify modal contants
    모달창에 mute, mteoff 상태 출력 안되도록 구현
    
    
    Former-commit-id: 05aea325e851076001930bbfdc433b6b0f74d5ac

--- frontend/tva/src/components/ActionModal/GymnasticsModal.tsx
+++ frontend/tva/src/components/ActionModal/GymnasticsModal.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:93bfcc08b8787b672df8e5304da57711f06c23c271df254b0d80e3e04d0b3fc8
-size 1524
+oid sha256:7271b5f0c0fa3bd437b3b1cadd7e0c80c9b15b4359c7c8fbfa4afb676c6a1f57
+size 1660
