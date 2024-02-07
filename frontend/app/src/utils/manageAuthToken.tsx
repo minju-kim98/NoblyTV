6698ba4c767293abb2be807356e5d904cbb352a4
강대수 <sore503@naver.com>
2024-02-07 17:38:39 +0900

    S10P12C103 :sparkles: feat : community update_savePost3
    Former-commit-id: 6ff82d8d622158d3df6e33b315cccee595a481a3

--- frontend/app/src/utils/manageAuthToken.tsx
+++ frontend/app/src/utils/manageAuthToken.tsx
@@ -1,3 +1,15 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:8d15f04d2421a53d3735be149c78b9125f1fb1c46828b3905e15d88a7542983e
-size 439
+import Cookies from 'js-cookie';
+
+function manageAuthToken({ handleNavigate }: { handleNavigate: () => void }) {
+  const authToken = Cookies.get('authToken');
+
+  // 토큰이 없으면 로그인 페이지로 이동
+  if (!authToken) {
+    handleNavigate();
+  } else {
+    // 토큰이 있으면 쿠키에 저장 (또는 갱신)
+    Cookies.set('authToken', authToken, { expires: 7 });
+  }
+}
+
+export default manageAuthToken;
