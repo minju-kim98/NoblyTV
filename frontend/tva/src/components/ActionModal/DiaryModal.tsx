cb1625abd9706b76b609d537bbe95450d181aec3
오민상 <flos9537@gmail.com>
2024-02-07 10:53:15 +0900

    #S10P12C103-656 ✨ feat: modify settimeout time
    모달창 사라짐 지연시간, 스트리밍 영상 시간 지연 관련 시간 수정, 더미 주석 제거
    
    
    Former-commit-id: c484adc66622b021162d81b92e461e56ab8b65d3

--- frontend/tva/src/components/ActionModal/DiaryModal.tsx
+++ frontend/tva/src/components/ActionModal/DiaryModal.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8a8d93c72b0284705f5f1d8cfb5d3233ca4e6a7fab0259ec9a27b395f3e2152e
-size 1159
+oid sha256:03240a0ab529856a154c2700a3fd68942a4bf04c3dd50c1c58ff655cab85df5a
+size 1158
