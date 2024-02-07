cb21b5010fcb422827b0b5219e2758bf3717236a
송강산 <rkdtks0816@naver.com>
2024-02-07 23:00:00 +0900

    #S10P12C103-306 ✨ feat : install pwa
    Former-commit-id: 95b386f29084c0234e827d5d407d7e5eb5ac7274

--- frontend/app/src/pages/ConnectTv/ConnectTv.tsx
+++ frontend/app/src/pages/ConnectTv/ConnectTv.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6b5b5b393f9207c65d73ba1040713d0b331d938986c5e7b78b4ab3b93aa0e9ce
-size 2392
+oid sha256:cbd5b8bb0a310810984a6d146c03cfc7f803053ec4e4910ad67073598ca8667d
+size 2332
--- frontend/app/src/pages/sign_up/Birth.tsx
+++ frontend/app/src/pages/sign_up/Birth.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:68c07a8c95f7ec5ab1afa22dc952e231d600e0aab10610d17d52fe3370f8f937
-size 3444
+oid sha256:59a7393e944e214fd96c414d6c149521546677e8f0ee1624a501514cd5146d7b
+size 3416
