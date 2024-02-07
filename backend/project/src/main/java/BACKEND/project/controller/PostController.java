20b2fbd5fe4598ce1c340f600d5da85335254707
강대수 <sore503@naver.com>
2024-02-07 17:47:34 +0900

    S10P12C103 :sparkles: feat : community fixerror_savePost
    Former-commit-id: 0082a33203dc587c53278c8959eb4956e50be24c

--- backend/project/src/main/java/BACKEND/project/controller/PostController.java
+++ backend/project/src/main/java/BACKEND/project/controller/PostController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:59f7668d68be476021e43d8725b201e934da228f32ded92f23914a88e3605b42
-size 2265
+oid sha256:c1f054f99484ef2a0d96f56e73be0ba21ccaf27fd46abf9be9e8a5db113d96be
+size 2302
