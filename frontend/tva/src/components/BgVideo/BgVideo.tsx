83e598b6e210f4d6862feccbbbf21fc5e3011128
오민상 <flos9537@gmail.com>
2024-02-07 10:45:01 +0900

    #S10P12C103-656 ✨ feat: add mute logic
    음소거 관련 로직 추가 및 수정
    
    
    Former-commit-id: 704b77c2293737a0eb306688fbd63640a45c28f7

--- frontend/tva/src/components/BgVideo/BgVideo.tsx
+++ frontend/tva/src/components/BgVideo/BgVideo.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8c19c16b935b5c1bbbbfdc86bf10e7b99d69737652b1fc36f232681ea6ab1dfd
-size 827
+oid sha256:8fc249b7db72751e24550d144675a374d691715e7efb023d032814a483da73b6
+size 1364
