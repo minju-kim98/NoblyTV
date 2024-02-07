778b2fbd899d0bd4fcc29d443bbd03481242cab2
송강산 <rkdtks0816@naver.com>
2024-02-07 23:00:00 +0900

    #S10P12C103-306 ✨ feat : install pwa
    Former-commit-id: 7d0faabf58e3414ca110c6ea97b02a071e22486c

--- frontend/app/src/main.tsx
+++ frontend/app/src/main.tsx
@@ -1,9 +1,3 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+version https://git-lfs.github.com/spec/v1
+oid sha256:4afef71a33b604186da62eba7655b0b3a1f11b5a257d6153ddf21c7d09c8f0dd
+size 547
