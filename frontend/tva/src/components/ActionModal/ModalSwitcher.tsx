ef49b7186f950d942ca616fc355756ec60803b24
오민상 <flos9537@gmail.com>
2024-02-07 12:12:28 +0900

    #S10P12C103-668 🐛fix: stop video debug
    체조 영상 끝나고 재생 멈추는거 컴포넌트 분리시켜서 구현
    
    
    Former-commit-id: 6175b8902e359f2ac6bef5d9192cb7694422ee64

---
+++ frontend/tva/src/components/ActionModal/ModalSwitcher.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:743850cb8c30fe49da237f307d78e81551c74a7dcfe8b6f6e9df614110ad0143
+size 535
---
+++ frontend/tva/src/pages/Overlay/OverlayR1.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:58e15e89a914c065bf33c5ea59623d4f4f015169cadc70815154ad0bba2b39ca
+size 2032
