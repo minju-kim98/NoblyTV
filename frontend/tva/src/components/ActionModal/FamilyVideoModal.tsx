b05a212654ee279f2dfc0da02616d59caec3fc2a
오민상 <flos9537@gmail.com>
2024-02-08 11:17:26 +0900

    #S10P12C103-682 ✨ feat: change url pass
    비디오 주소 처리 및 경로 코드 수정
    
    
    Former-commit-id: b0aa6100c2591db466f99044b3a1119fd26e6d0e

--- frontend/tva/src/components/ActionModal/FamilyVideoModal.tsx
+++ frontend/tva/src/components/ActionModal/FamilyVideoModal.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ed7a9e3b1008bf0bf883e0a3ff1d0bb4b641d516488783cf174c526afcf7d1ca
-size 2010
+oid sha256:edd5c7ba21f04f66cd1577b7166ad53581c05ca65c1ab678a1d5e35745b689c6
+size 2277
