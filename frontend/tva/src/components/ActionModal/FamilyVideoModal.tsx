5050294ce0dffc7b9714ce2f12f8ec909eef05d6
오민상 <flos9537@gmail.com>
2024-02-08 12:17:30 +0900

    #S10P12C103-682 ✨ feat: finish connect socket and test motion
    소켓 통신 테스트 및 비디오 출력 확인 완료
    
    
    Former-commit-id: 5c9e2bd622d98eb6f937302dbaf41eccf9f6d301

--- frontend/tva/src/components/ActionModal/FamilyVideoModal.tsx
+++ frontend/tva/src/components/ActionModal/FamilyVideoModal.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:edd5c7ba21f04f66cd1577b7166ad53581c05ca65c1ab678a1d5e35745b689c6
-size 2277
+oid sha256:e418650c461c4f2fd0a283252a3ab27a5fd5404d27e26e99b7c4830a56660f44
+size 3246
