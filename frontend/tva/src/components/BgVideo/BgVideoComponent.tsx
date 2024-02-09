1eea6cf74f0dea1a21ae8499f10a2f7da731c9d0
오민상 <flos9537@gmail.com>
2024-02-09 13:11:07 +0900

    #S10P12C103-684
    Former-commit-id: 5daaee4bcf1d512e68511b6acf2ec6671497fe03

--- frontend/tva/src/components/BgVideo/BgVideoComponent.tsx
+++ frontend/tva/src/components/BgVideo/BgVideoComponent.tsx
@@ -1,12 +1,3 @@
-import React from 'react';
-import BgVideo from './BgVideo';
-
-interface BgVideoComponentProps {
-  currentMode: string;
-}
-
-const BgVideoComponent: React.FC<BgVideoComponentProps> = React.memo(
-  ({ currentMode }) => <BgVideo currentMode={currentMode} />,
-);
-
-export default BgVideoComponent;
+version https://git-lfs.github.com/spec/v1
+oid sha256:61c44507311333d26e7d531605a18282cf20b6ead5384cf9df691d3046b4b4bb
+size 396
