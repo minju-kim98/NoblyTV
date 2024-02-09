7768d31f0063a9b54e68bde5e6694d0873abbd49
오민상 <flos9537@gmail.com>
2024-02-10 00:31:19 +0900

    #S10P12C103-684 ✨ feat: not active option - autoplay gymnastic youtube video
    Former-commit-id: 9fcb2efb909e3f1dc371c63926eaee01bdb118c1

--- frontend/tva/src/pages/gymnastics/Gymnastics.tsx
+++ frontend/tva/src/pages/gymnastics/Gymnastics.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6e793b9179226ee8a7bf6e72d631585e85eaf08b847358d2fe4f24c0020c741b
-size 1554
+oid sha256:a7f09b26e683bded74813863a4749d31288193bbf2335881cf4169f08f51fce8
+size 1557
