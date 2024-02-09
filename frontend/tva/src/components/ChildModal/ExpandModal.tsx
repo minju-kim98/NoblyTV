74136205127ec364bc8b90390ee0250255e634c8
오민상 <flos9537@gmail.com>
2024-02-09 17:32:03 +0900

    #S10P12C103-684 ✨ feat: create motion and css
    모달창 확대 후 가족영상 재생
    가족영상 끝나면 stop 송신 및 가족영상 : 메시지+손주사진 = 1 : 1 비율로 css 적용
    전체화면일 때 메시지+손주사진은 슬라이드 형태로 등장하도록 추가
    
    
    Former-commit-id: 348e14271bdd3fc6f7644a0cf41c68b609caf148

--- frontend/tva/src/components/ChildModal/ExpandModal.tsx
+++ frontend/tva/src/components/ChildModal/ExpandModal.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d0b1490b164aa7239c884c5a234aea412ca54ec13eb26805d68b4ba4faaef13b
-size 1672
+oid sha256:689b1b5912fb892f783d759d27e9ddbba825fd2942ab62cdcb6fecbac4524799
+size 1675
