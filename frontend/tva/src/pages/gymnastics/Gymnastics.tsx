15f539f57cdc192af4c81f4345f6a31de05ff0bf
오민상 <flos9537@gmail.com>
2024-02-10 17:28:00 +0900

    #S10P12C103-686 ♻️ refactor: apply constant socket url( BASE_URL:SOCKET_PORT)
    소켓 통신 관련 url을 변수화 하여 관리
    
    
    Former-commit-id: 3f32443023d6d2841415a01217c352b0c1b8c222

--- frontend/tva/src/pages/gymnastics/Gymnastics.tsx
+++ frontend/tva/src/pages/gymnastics/Gymnastics.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a7f09b26e683bded74813863a4749d31288193bbf2335881cf4169f08f51fce8
-size 1557
+oid sha256:b6b1460e0d8f96029f6ef3e355c866a76fc73ccb33cd400cc42e7bbae3cb538a
+size 1621
