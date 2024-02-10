44546e56b1b8f2b26b2245cf9e792ced0b90246e
오민상 <flos9537@gmail.com>
2024-02-10 17:27:16 +0900

    Revert "#S10P12C103-684 ♻️ refactor: apply constant socket url( BASE_URL:SOCKET_PORT)"
    This reverts commit 182124fb33dc24ba60735daca2d99734d4820869 [formerly c8a114f514bf1244b201ac082a83cce1d4533508].
    
    
    Former-commit-id: c7398d5478dd2d70fc4341e56ecb5ff19d2bff56

--- frontend/tva/src/components/ChildModal/ChildModalStyles.tsx
+++ frontend/tva/src/components/ChildModal/ChildModalStyles.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a10b35148813716eb05ea5b72203e4e875d685b945b99b0bbede292108e63c44
+oid sha256:5920e7f141c497ed05965377a16c7bd5ca2bce1833c7c047a8f074b2ced48544
 size 2678
