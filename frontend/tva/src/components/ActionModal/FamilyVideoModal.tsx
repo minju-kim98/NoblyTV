20ecc9071bfb699bdcb62a5bb61e6ac2d5e6b500
오민상 <flos9537@gmail.com>
2024-02-11 17:06:15 +0900

    #S10P12C103-686 ✨ feat: modify familyViedos logic and css
    Former-commit-id: dfc32781aabd5c89ec1c38b93ca43975fb5b1de1

--- frontend/tva/src/components/ActionModal/FamilyVideoModal.tsx
+++ frontend/tva/src/components/ActionModal/FamilyVideoModal.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0a9059c2757a6b14ccf542ec9ccbd6c49f31ffe98bfa60dd6fc8ddcb49bfdb6e
-size 2430
+oid sha256:95415b21ce7cdf495f1bd430aa61bcc72d63cfc972fea3484ef7c63e72a67282
+size 2539
--- frontend/tva/src/pages/Overlay/Overlay.tsx
+++ frontend/tva/src/pages/Overlay/Overlay.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1af542ed94b0de6cfa9d4e2ae96b8d6e52fa2a55f5939ffa2e0155201cbb8a21
-size 2874
+oid sha256:9a0687a8d74d59b651191952d17c29f3183107065c541a1dd9578e88ac8c446d
+size 2871
