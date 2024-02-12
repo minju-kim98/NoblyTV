f1a73897406360ea80fad683df3df4875d6f966f
김민주 <ducco705@snu.ac.kr>
2024-02-12 14:58:42 +0900

    #S10P12C103-673 🐛 fix: added ultrasonic socketio feature
    Former-commit-id: a3e5a7e67a35bde91d47dd482449987eaf2157ca

--- embedded/videoSummarization/main.py
+++ embedded/videoSummarization/main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:164dc05d3108d5ed8b778731b17cd88a3213693eae77b1f748e077a5e5fa1e58
-size 2673
+oid sha256:27c10ed015a5937abbcea689a8ecd9b277f0508e2972b698f1fe3ccc1c6c6e25
+size 2993
