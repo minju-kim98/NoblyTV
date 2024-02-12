0318a00115e68f8899edca25bdf9be4a7e84540d
오민상 <flos9537@gmail.com>
2024-02-12 21:25:19 +0900

    #S10P12C103-687 ✨ feat: css modify
    ExpandModal에서 텍스트 문자열 자동 줄바꿈 구현
    
    
    Former-commit-id: ac6d43d84ff0320f2c4abb3fb03b09a846806b5a

--- frontend/tva/src/components/ChildModal/ChildModalStyles.tsx
+++ frontend/tva/src/components/ChildModal/ChildModalStyles.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:920e807f613e0167fa3e031ff8c327d13eb37e94e1e959f732e8995fc27f1003
-size 2533
+oid sha256:ef036549414dbb7e578f0c240a842ae73cbd081513002c1d32fda08910a1418d
+size 2584
