978878001c6700db2ede6f1d99dd12ea4d15287b
이지호 <jeho1129@naver.com>
2024-02-12 17:50:55 +0900

    #S10P12C103-688 :recycle: refactoring: TvCode Logic
    Former-commit-id: ca5242ff82b35505cc1f1cf1218a8fba6976106b

--- backend/project/src/main/java/BACKEND/project/controller/TvLoginController.java
+++ backend/project/src/main/java/BACKEND/project/controller/TvLoginController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c69587e63f8580ca986ebb7b8a8ac11272505bb7276e18a2d1ac5243d917e96b
-size 1907
+oid sha256:0740471c09f7e51bdedd4bd9d59eda24b058c987d6a4c5cf540300dbba45c6d1
+size 2431
