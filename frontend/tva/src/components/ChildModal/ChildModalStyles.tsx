30a34092fc73cf3e46f8d9264437f90bb6d3b80e
오민상 <flos9537@gmail.com>
2024-02-12 21:16:41 +0900

    #S10P12C103-687 ✨ feat: css modify
    EM에서 넘어온 텍스트가 길어질 경우 모달창 반응형으로 움직이게 수정
    
    
    Former-commit-id: cbba05a9f84071eeb8fbac3b9da028f2f189cf33

--- frontend/tva/src/components/ChildModal/ChildModalStyles.tsx
+++ frontend/tva/src/components/ChildModal/ChildModalStyles.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d6ad1b93c698ffe3d9333c6bfa8401ba6d1052e1c42f9445feeceb7175ee8e11
-size 2476
+oid sha256:920e807f613e0167fa3e031ff8c327d13eb37e94e1e959f732e8995fc27f1003
+size 2533
