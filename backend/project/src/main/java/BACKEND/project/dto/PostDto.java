c471529654c47dbce096cdc24c71da767185f961
KangDaeSu <sore503@naver.com>
2024-02-12 15:07:38 +0900

    S10P12C103-543 :sparkles: feat : post code update
    Former-commit-id: f745105759afd597b44d9638f0bfa4ba76b11709

--- backend/project/src/main/java/BACKEND/project/dto/PostDto.java
+++ backend/project/src/main/java/BACKEND/project/dto/PostDto.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3d610b872afad9833e46f4ab2f85f9ce50538a315714d39f033949089785ddf2
-size 736
+oid sha256:3e456f60951468145150247a9020fdcbebd24bf89b3235a47a4a4c3bb381bcca
+size 766
--- backend/project/src/main/java/BACKEND/project/repository/PostRepository.java
+++ backend/project/src/main/java/BACKEND/project/repository/PostRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e8df4734b64c39964b34a2ae956242fd3b3d726076e3d69c5a6441749b5df4d8
-size 536
+oid sha256:e64a72960357dc34d65e6a4c64cd5ec469c4d21d5dc24376c797b5d6603953ac
+size 351
