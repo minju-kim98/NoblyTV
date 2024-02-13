64dc81ae349445c5a52f5e35a3d15fa0d01e6d4b
오민상 <flos9537@gmail.com>
2024-02-13 15:47:09 +0900

    #S10P12C103-690 ♻️ refactor : gymnastic video in streaming video time change 10000 --> 7000ms
    Former-commit-id: f1e87ed95e7039731d46c637edb90da004bd9034

--- frontend/tva/src/pages/gymnastics/Gymnastics.tsx
+++ frontend/tva/src/pages/gymnastics/Gymnastics.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4febe2c980cb33d21fbad4a5a3505c070ad19db01d53e64b6de24fe4d8148d5e
+oid sha256:dbae983b31560b8188551204ba9a9fd3efa597646cb75cb4d728399fd6c42adb
 size 3396
