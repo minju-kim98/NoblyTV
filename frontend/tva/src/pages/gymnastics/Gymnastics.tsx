59682cd495a06ed41640590e5d864d9cbc78bbd9
오민상 <flos9537@gmail.com>
2024-02-13 16:57:26 +0900

    #S10P12C103-698 ✨ feat: gymnastic modal motion develop
    체조 영상 및 모달창 모션 부드럽게 동작하도록 구현
    
    
    Former-commit-id: 5f5488974bc72b1df4d4f558a6c3a2209cb2412a

--- frontend/tva/src/pages/gymnastics/Gymnastics.tsx
+++ frontend/tva/src/pages/gymnastics/Gymnastics.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c27387e6cf6269e3a2c0ec96f8d28e598a29fdd59b1886e11b5ae4ea37f24519
+oid sha256:9d195f2d6eddf6a9ad47a6bfeff69f529033880ea3379b4b246225f65f1752f6
 size 3396
