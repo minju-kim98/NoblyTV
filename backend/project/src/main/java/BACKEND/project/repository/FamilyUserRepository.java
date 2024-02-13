9e7827efc9d1a9cab19831380988305115a668e3
강대수 <sore503@naver.com>
2024-02-13 16:39:12 +0900

    S10P12C103 :sparkles: feat : family upload post update
    Former-commit-id: 00e3b95407e5d5f6186e0edcad9913c603786285

--- backend/project/src/main/java/BACKEND/project/repository/FamilyUserRepository.java
+++ backend/project/src/main/java/BACKEND/project/repository/FamilyUserRepository.java
@@ -1,14 +1,3 @@
-package BACKEND.project.repository;
-
-import BACKEND.project.domain.FamilyUserInfo;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import java.util.Optional;
-
-@Repository
-public interface FamilyUserRepository extends JpaRepository<FamilyUserInfo, Long> {
-    Optional<FamilyUserInfo> findByUserId(String userId);
-
-    Optional<FamilyUserInfo> findById(Long id);
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:4d8017222260e5d3c00cc4dd54dbd6ff6ca297f9270d36bf7dd03ac1f94078a5
+size 477
