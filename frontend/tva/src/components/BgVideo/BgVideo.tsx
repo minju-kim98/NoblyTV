fa70606fad832b8a7757e187f370b81a3eafd857
오민상 <flos9537@gmail.com>
2024-02-13 15:24:47 +0900

    #S10P12C103-690 ♻️ refactor : chage css and change gymnastic code
    배경이 되는 비디오영상 컨트롤바 숨김
    체조영상은 유튜브에서 로컬 영상 보이도록 임시 수정
    
    
    Former-commit-id: 4dd486f968b44bae7675058e4eb21bb8234e63fd

--- frontend/tva/src/components/BgVideo/BgVideo.tsx
+++ frontend/tva/src/components/BgVideo/BgVideo.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0b0f3ae1ffda06e1f6a92ceb2b100a96b7ef70c04d65d1ce9373c92b917c6b9a
-size 1837
+oid sha256:83229e22f4fe02788d1fb0673adfc1ac1f6e13cbda4e6c84266bb7190010675f
+size 1828
--- frontend/tva/src/pages/gymnastics/Gymnastics.tsx
+++ frontend/tva/src/pages/gymnastics/Gymnastics.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d1ee4daf0d185600d3d7b6b93cd0dbbadd52f1c9dd5355afae57ada032196b36
-size 1621
+oid sha256:4febe2c980cb33d21fbad4a5a3505c070ad19db01d53e64b6de24fe4d8148d5e
+size 3396
