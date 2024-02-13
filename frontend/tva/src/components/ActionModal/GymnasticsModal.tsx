c41828986ff3c91a810a08d797a880ddad855703
오민상 <flos9537@gmail.com>
2024-02-13 17:58:19 +0900

    #S10P12C103-698 ✨ feat: Gymnastic video motion add
    Former-commit-id: e8aef7f6f235da11a7d76801edf12183c8671916

--- frontend/tva/src/components/ActionModal/GymnasticsModal.tsx
+++ frontend/tva/src/components/ActionModal/GymnasticsModal.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c3ba596b70f4ae47bf926b02d17f53a5d2d6055bc594caf6f610c5b21d6f2d35
-size 2112
+oid sha256:1ef079cc27379d7da3915c00629aad535ff0ed003f1f64a61be1c003ea9aebf7
+size 2150
