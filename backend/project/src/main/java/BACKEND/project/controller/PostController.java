64981cd84c94d35918889e5e09df95d20ee5d6f7
이지호 <jeho1129@naver.com>
2024-02-13 17:13:40 +0900

    #S10P12C103-711 :recycle: refactoring: Long -> String
    Former-commit-id: 18038cbf937eead6e9e6f952a13d91ff3e2f0784

--- backend/project/src/main/java/BACKEND/project/controller/PostController.java
+++ backend/project/src/main/java/BACKEND/project/controller/PostController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1817c1218c84c9a4513c0f8cf9cdc81083e743a9473ac4a671a3d24a737f6a52
-size 3978
+oid sha256:7306d0fe5e1bf026aa4e318158e0fa367769d7bae814c8af31c528d1bc99c9be
+size 3980
