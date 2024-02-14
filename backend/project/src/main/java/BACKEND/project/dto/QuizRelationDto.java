37ccb62a7721303e98ae75fbb078e61c4bbcbe19
현상균 <htts1049@gmail.com>
2024-02-14 12:10:47 +0900

    #S10P12C103-340 📝 docs: 3D Printing Hat
    Former-commit-id: 311720d35697d36a3a3cdc810161421eae1cdf65

--- backend/project/src/main/java/BACKEND/project/dto/QuizRelationDto.java
+++ backend/project/src/main/java/BACKEND/project/dto/QuizRelationDto.java
@@ -1,14 +1,3 @@
-package BACKEND.project.dto;
-
-import com.fasterxml.jackson.annotation.JsonInclude;
-import lombok.Data;
-
-@Data
-@JsonInclude(JsonInclude.Include.NON_NULL)
-public class QuizRelationDto {
-
-    private Long id;
-    private Long quizId;
-    private Long userId;
-    private boolean isCorrect;
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:641c0fc4cf74313307808a1453398f7fca0413fb565c79fe2be97c3a0282c5a7
+size 289
