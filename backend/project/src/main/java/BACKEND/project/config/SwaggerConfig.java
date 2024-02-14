c1fadcf8b83fb4fd189f1fa79f259169e363ec31
이지호 <jeho1129@naver.com>
2024-02-14 10:39:40 +0900

    #S10P12C103-713 :sparkles: feat: Calculating TV Watching Times + Code Refactoring
    Former-commit-id: 1050ccd88721b80ec86962305520ff1a8d604c02

--- backend/project/src/main/java/BACKEND/project/config/SwaggerConfig.java
+++ backend/project/src/main/java/BACKEND/project/config/SwaggerConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a71b3d18dbd6c68f2cac67aa3fe0c86062da767fdf02122734542e3c7c9fd73a
-size 1230
+oid sha256:5387f62227d13275764195269ef48a9fafbaaa5829a3a91dc4d5e958e322c3fe
+size 1226
--- backend/project/src/main/java/BACKEND/project/controller/OldUserJoinController.java
+++ backend/project/src/main/java/BACKEND/project/controller/OldUserJoinController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b54520ea69b3cf7aa78334b54ed18df1b5a73a1095d600966cf7a4c53c7776ed
-size 1915
+oid sha256:f4c38b4ce17fee292d76cdea9fefdbbd1e25bae4246a7247826c3f6f634b2a1d
+size 1874
