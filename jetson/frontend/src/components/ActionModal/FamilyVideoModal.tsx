db3d1e793d0744534dbc2c9a00fe058e0c5dd5db
오민상 <flos9537@gmail.com>
2024-02-14 17:54:51 +0900

    #S10P12C103-715
    Former-commit-id: edb4378e263f3fde984f1a7f7dc02c1eed0468fb

--- jetson/frontend/src/components/ActionModal/FamilyVideoModal.tsx
+++ jetson/frontend/src/components/ActionModal/FamilyVideoModal.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:01c759034ac26f530ccd4cba90ac1a99b261ed2dbd72ec1126b9ce34d970c801
-size 2422
+oid sha256:ffbf2421b14768a1e3ccb55b049a2b8a9d3438139988202d0d8b03eaf061d88b
+size 2448
--- jetson/frontend/src/components/ChildModal/ExpandChildModal.tsx
+++ jetson/frontend/src/components/ChildModal/ExpandChildModal.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4dcc9181e9096697a127255b274cc00bd59dd689f8932df66dcd75d9a721211f
-size 4144
+oid sha256:4b9063de92d38500dd12c314e55352d521dd77a5659285b561f7adb1a7a359ac
+size 4322
