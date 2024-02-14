e85d1d5410b6f77049ca109483e27d919b16c49b
송강산 <rkdtks0816@naver.com>
2024-02-14 15:13:52 +0900

    ♻ refactor : file refactor
    Former-commit-id: 8b37dced9944d592f2d2a74cdea3a8bb4dc4d0a7

--- jenkins/init.sh
+++ jenkins/init.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f9f44c681fac74172d06f98e74aca3d55c67313348060f327a38cf9894bca6c2
-size 843
+oid sha256:7b6f37ca7beafafdc7fadbbb9d9071fa9b10a6511a80dab4f45c15f825571031
+size 873
