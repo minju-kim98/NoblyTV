719dc16a6b8e7b77d228acc2dbdb32b55a2f85fc
오민상 <flos9537@gmail.com>
2024-02-15 14:39:23 +0900

    #S10P12C103-681 ✏️ comment: modify default TVcode
    Former-commit-id: dec147ca3cca73e118a4415e15e4d6b1f1ab9f1e

--- jetson/frontend/src/pages/SignInTV/SignInTV.tsx
+++ jetson/frontend/src/pages/SignInTV/SignInTV.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4cafacc0653715140c6d411d69dc009afed9785c1d116e89ee2b9a5af1df0854
-size 2533
+oid sha256:21e571b866e9d3596e6dd1ff02679ab3c9bba01aef8d7ed625566be89b656197
+size 2534
