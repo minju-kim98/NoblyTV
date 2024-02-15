9b8d2bc62ff034c391c21e3be9d816ccf0483fa1
이지호 <jeho1129@naver.com>
2024-02-15 10:31:38 +0900

    #S10P12C103-714 ✨ feat: README + Outputs Files 5
    Former-commit-id: e613ec33594640bf23aaf1607c62891478e47313

--- jetson/frontend/src/components/ChildModal/StreamingModal.tsx
+++ jetson/frontend/src/components/ChildModal/StreamingModal.tsx
@@ -1,3 +1,13 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:ff7ab929a127e5f6323568c36bb6bd565fa8bf4264697c7d626d6a203d2b2d9e
-size 342
+import { ChildModalVideoBG } from './ChildModalStyles';
+
+function StreamingModal() {
+  const streamUrl = 'http://192.168.100.245:5000/video_feed';
+
+  return (
+    <ChildModalVideoBG>
+      <img src={streamUrl} alt="비디오 스트림" style={{ width: '100%' }} />
+    </ChildModalVideoBG>
+  );
+}
+
+export default StreamingModal;
