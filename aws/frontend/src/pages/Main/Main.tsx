b034aa58c0073ba064bdb12c70f49f442568ff11
송강산 <rkdtks0816@naver.com>
2024-02-15 15:05:58 +0900

    ✨ feat : create alarm
    Former-commit-id: fd06e8817c92119dad7f47fbfe5118b822cde3b1

--- aws/frontend/src/pages/Main/Main.tsx
+++ aws/frontend/src/pages/Main/Main.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b518791b6d09ef6114df33b8b416d1fc8bb6d491fbc026ef01c806c213958f39
-size 4573
+oid sha256:0b60276e61347331e85510538c62587d97adf56d0ba3fd502b0a9f1dca76f003
+size 6241
