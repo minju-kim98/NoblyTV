da69fe1a69fcf6b2dae7ed7fe7af180ce7de4f39
오민상 <flos9537@gmail.com>
2024-02-15 11:37:51 +0900

    #S10P12C103-681 ✨ feat: add socker.emit code
    소켓 송신 코드 추가
    
    
    Former-commit-id: e31c0f2ec5c2ef1dd01831a3c3c955f7ad3a5b66

--- jetson/frontend/src/pages/SignInTV/SignInTV.tsx
+++ jetson/frontend/src/pages/SignInTV/SignInTV.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bea6f807d09a6b4ce6c815efc0f33f310cfbad30573c806d134cb0c8191cb960
+oid sha256:6afe499bf9f40bdcaae57fe03be4de3b3bfd26bbbeafd291033d96afdb69a280
 size 1881
