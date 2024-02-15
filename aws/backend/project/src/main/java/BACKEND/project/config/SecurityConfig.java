ad7ae596b7f53146e82a8174c05293b31cfdaa14
송강산 <rkdtks0816@naver.com>
2024-02-15 10:46:19 +0900

    Update SecurityConfig.java
    Former-commit-id: 8f00f46177cd89a89f5af131e0425c3d7d079445

--- aws/backend/project/src/main/java/BACKEND/project/config/SecurityConfig.java
+++ aws/backend/project/src/main/java/BACKEND/project/config/SecurityConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cbd7e48104da6c12edeff7039115bdfc3d551fce6096bd633d9982f5bb0474a3
-size 4034
+oid sha256:7def563139c992e4473f8e7939690e830b0ff61c67237b8000f5a9d8ef15be30
+size 4096
