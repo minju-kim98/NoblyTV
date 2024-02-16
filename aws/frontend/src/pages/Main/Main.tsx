e64e68a4bf7420ec8d290d68ba0a6ec9af580edd
송강산 <rkdtks0816@naver.com>
2024-02-16 10:02:29 +0900

    Update Main.tsx
    Former-commit-id: ffc2bbb7acf8ee9642f8b10db7ef57e11d022372

--- aws/frontend/src/pages/Main/Main.tsx
+++ aws/frontend/src/pages/Main/Main.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b0b6439876478880039ca1f3cd1f0f643e1149e4bdc91784793c63ba12b26158
-size 6828
+oid sha256:d56e6a530a5b794877b3ffb9967765b6cbea07599c8c98c9f616708a0fc14f0b
+size 6826
