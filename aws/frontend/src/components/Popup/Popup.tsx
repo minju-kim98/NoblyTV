baaa94b01281b42a1afc657530c7f21fe33ec750
송강산 <rkdtks0816@naver.com>
2024-02-16 09:20:34 +0900

    🐛 fixed : app fixed
    Former-commit-id: ac6df158f598682410f5b8b292420b7f7737f26f

--- aws/frontend/src/components/Popup/Popup.tsx
+++ aws/frontend/src/components/Popup/Popup.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6eaf865e417e2814088beca2b10360a41fe84b5a1d183dd1cccf059c454706ff
-size 728
+oid sha256:ac2d20b2de63341fb574c127a0244dcd6a2679c1e58adc468a11ea3e67361d60
+size 765
